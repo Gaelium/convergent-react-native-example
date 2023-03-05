d57d8d5dd8f6c47e1194865fdc94939120ca99cd
Connor P <38620265+Gaelium@users.noreply.github.com>
2023-03-05 10:58:42 -0600

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBY9He9XbO7pzQ-nAcskYB9CYk-yZ1EN0o",
-  authDomain: "convergent-test.firebaseapp.com",
-  projectId: "convergent-test",
-  storageBucket: "convergent-test.appspot.com",
-  messagingSenderId: "545802229360",
-  appId: "1:545802229360:web:91180acf1964d249ebc6b5",
-  measurementId: "G-50REV2L3WD",
+  //Your firebase config goes here!
 };
 
 // Initialize Firebase
